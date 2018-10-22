9f3d9feed9f9a23546479a613f601735bcf546a8
iPoe <leonardosawi12@gmail.com>
2018-10-22 12:01:29 -0500

    The beginning
    Primeras ideas para la creación del proyecto final de agra

---
+++ Reddit_tree.py
@@ -0,0 +1,12 @@
+from sys import stdin
+
+"""AQUI DEBE IR LA CLASE ARBOL {indent_arrow}{body}[{author}|{ups}|{downs}|{comment_id}|{date}]"""
+
+
+
+
+""" AQUI VOY A HACER EL PARSING ¿AL LEER UNA LINEA LLAMAR RECURSIVAMENTE HASTA ENCONTRAR LA HOJA?  """
+
+def main():
+
+main()
