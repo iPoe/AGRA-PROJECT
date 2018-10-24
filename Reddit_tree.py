10137a73ca33a14beb3421d9b73f024731bb551f
iPoe <leonardosawi12@gmail.com>
2018-10-24 17:22:41 -0500

    Cleaning code

--- Reddit_tree.py
+++ Reddit_tree.py
@@ -30,8 +30,7 @@ def leerinfo(id):
 	elif id == 0:
 		ans = 0
 		while INPUT[I]!= RAYA and has_next():
-			ans,I = int(chr(INPUT[I]))+ans*10,I+1
-			
+			ans,I = int(chr(INPUT[I]))+ans*10,I+1			
 		I+=1
 		return ans
 
@@ -64,46 +63,4 @@ def main():
 main()
 
 
-
-
-
-
-
-
-
-
-
-"""AQUI DEBE IR LA CLASE ARBOL {indent_arrow}{body}[{author}|{ups}|{downs}|{comment_id}|{date}]"""
-#class Ftree(object):
-                 #Que atributos poner¿?
-""" AQUI VOY A HACER EL PARSING ¿AL LEER UNA LINEA LLAMAR RECURSIVAMENTE HASTA ENCONTRAR LA HOJA?  """
-"""
-def parseline():
-	line = stdin.readline().strip()
-	pre = list()
-	while len(line)!=0:
-		read_line(line,pre)
-		parseline()
-	print(pre)
-
-def read_line(line,lista):
-	global n
-	token = lista[0]
-	i = 0
-	cnt = 0
-	while token !='>':		#Aqui estoy calculando la profundidad del nodo en el arbol
-		i+=1
-		token = lista[cnt+2]
-	#Ahora debo leer al reves
-	n = len(line)-2 #Aqui consumo el parentesis, digamos que me lo salto
-	token1 = lista[n]
-	date,comment_id,downs,ups, downs, author,body = None, None, None, None, None, None, None	
-	while token1 !=	RCORCHETE:
-		#token1 = lista[n]
-		date = []
-		while token1 != LINE:
-			date.append(token1)			
-			n = n-1
-			token1 = lista[n]
-"""
 
