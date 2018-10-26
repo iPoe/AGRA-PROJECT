c46024df4dc34471341449829913f69def096116
iPoe <leonardosawi12@gmail.com>
2018-10-26 11:24:27 -0500

    Almost done
    Aun falta contar la cantidad de comentarios para cada autor

--- Reddit_tree.py
+++ Reddit_tree.py
@@ -1,23 +1,24 @@
 from sys import stdin
 class Tree(object):
-	def __init__(self,data,A=[]):
+	def __init__(self,data,A,up,down):
 		self.__len=len(A)
 		self.__tree = A
 		self.__data=data
-		self.__likes=0
-		self.__dislikes=0
+		self.__likes=up
+		self.__dislikes=down
+		self.__Tlikes=up
+		self.__Tdislikes=down
 	def contarlikes(self):
-		if self.__len(self.__tree)==0:
-			return (self.__likes,self.__dislikes)
+		if self.__len==0:
+			return (self.__Tlikes,self.__Tdislikes)
 		else:
 			likes,dislikes=self.__likes,self.__dislikes
-			for i in __tree:
-				S=i.contarlikes()
-				likes+=S[0]
-				dislikes+=S[1]
+			for i in self.__tree:
+				S=i.contarlikes(); likes+=S[0] ; dislikes+=S[1]
+		self.__Tlikes,self.__Tdislikes=likes,dislikes	
 		return (likes,dislikes)
 	def imprimir(self):
-		print(self.__data)		
+		print(self.__Tlikes,self.__Tdislikes)		
 		for i in self.__tree:
 			i.imprimir()
 def parse_line():
@@ -51,25 +52,35 @@ def create():
 						childs.append(t)
 			I=j
 
-			Arbol=Tree(PREORDER[j][1],childs)		
+			Arbol=Tree(PREORDER[j][1],childs,PREORDER[j][2],PREORDER[j][3])		
 						
 	return Arbol
 
 def main():
 	global line, PREORDER
 	line = stdin.readline().strip()
-	PREORDER = list() # suponiendo un foro
+	PREORDER = list() # suponiendo un foro	
 	while(len(line) != 0):
 		prof, body, nombre, ups, dwns, id, date = parse_line() #cada vez que haya una profundidad 0 debo reiniciar el preorden y el arbol
-		PREORDER.append((prof,id))		
+		PREORDER.append((int(prof),id,int(ups),int(dwns)))		
 		line = stdin.readline().strip()
+		"""
+		if authors.get('nombre')!=None:
+			authors['nombre']=1
+		else:
+			authors['nombre']+=1
+		"""
+
 	Arbol=create()
-	Arbol.imprimir()
-	"""
+	
+	Arbol.contarlikes()
 	for id in PREORDER[:len(PREORDER)-1]:
-		print(id, end = ' ')
+		print(id[1], end = ' ')
 	print(PREORDER[-1])
-	"""
+
+	Arbol.imprimir()
+	
+
 main()
 
 
